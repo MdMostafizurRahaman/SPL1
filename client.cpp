3999667e3403b12e8bccf8db57a803c61760b04f
MD MOSTAFIZUR RAHAMAN <121539038+MdMostafizurRahaman@users.noreply.github.com>
2023-04-05 01:16:50 +0600

    client one to one edited

--- client.cpp
+++ client.cpp
@@ -1,94 +1,36 @@
-/*
-filename server_ipaddress portno
-argv[0] -> file name
-argv[1] -> server_ipaddress
-argv[2 -> portno
-*/
+#include <netinet/in.h> //structure for storing address information
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h> //for socket APIs
+#include <sys/types.h>
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<netdb.h>
-
-int main(int argc, char *argv[])
+int main(int argc, char const* argv[])
 {
-    int portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    char buffer[255];
+	int sockD = socket(AF_INET, SOCK_STREAM, 0);
 
-    if(argc < 3)
-    {
-        printf("Usage %s hostname port\n", argv[0]);
-        exit(0);
-    }
-    
-    portno = atoi(argv[2]);
-    
-    int sock_desc = socket(AF_INET, SOCK_STREAM, 0);
-	if(sock_desc < 0)
-	{
-		printf("NO socket is created!\n");
-	}
-	else
-	{
-		printf("Socket is created!\n");
+	struct sockaddr_in servAddr;
+
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_port
+		= htons(9001); // use some unused port number
+	servAddr.sin_addr.s_addr = INADDR_ANY;
+
+	int connectStatus
+		= connect(sockD, (struct sockaddr*)&servAddr,
+				sizeof(servAddr));
+
+	if (connectStatus == -1) {
+		printf("Error...\n");
 	}
 
-    server = gethostbyname(argv[1]);
-    if(server == NULL);
-    {
-        fprintf(stderr, "Error, no such host...\n");
-    }
+	else {
+		char strData[255];
 
-    bzero((char*)&serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
+		recv(sockD, strData, sizeof(strData), 0);
 
-    if(connect(sock_desc, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("Connection failed\n");
-    }
-    else
-    {
-        printf("Connected....\n");
-    }
-    
-    while(1)
-    {
-        bzero(buffer, 255);
-        fgets(buffer, 255, stdin);
-       n = write(sock_desc, buffer, strlen(buffer));
-       if(n < 0)
-       {
-            printf("Error on writing\n");
-       }
-       
+		printf("Message: %s\n", strData);
+	}
 
-       bzero(buffer, 255);
-       n = read(sock_desc, buffer, 255);
-       if(n < 0)
-       {
-            printf("Error on reading\n");
-       }
-       
-       printf("Server:%s", buffer);
-       int i = strncmp("Bye", buffer, 3);
-       if(i == 0)
-       break;
-    }
-    
-
-    
-
-
-
-
-
-
+	return 0;
 }
+
