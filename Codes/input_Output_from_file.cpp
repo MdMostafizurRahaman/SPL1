ff807cc3de1a2ee7e5696c8ddf3e1306f741d447
MD MOSTAFIZUR RAHAMAN <121539038+MdMostafizurRahaman@users.noreply.github.com>
2023-04-20 11:16:04 +0600

    input from file

---
+++ Codes/input_Output_from_file.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string num;
+    freopen("in.txt", "r", stdin);
+    cin >> num;
+    cout << "The number is: " << num << endl;
+    return 0;
+}
