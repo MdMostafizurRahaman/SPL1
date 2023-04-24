3209d705512cc0ebae0bb0d252c8e4ca65a78e80
Mostafizur <bsse1320@iit.du.ac.bd>
2023-04-24 14:59:21 +0600

    check is multithread working!

--- Codes/thread.cpp
+++ Codes/thread.cpp
@@ -3,12 +3,21 @@
 #include<unistd.h>
 #include<pthread.h>
 
+int a = 0;
+
 void *thread(void * th)
 {
     int *id = (int *)th;
-    sleep(3);
-    printf("First thread use:%d",*id);
-    printf("\n");
+    static int b = 0;
+
+    a = 10;
+    b = 20;
+    sleep(1);
+
+    printf("The id of thread is:%d\n", *id);
+    printf("The value of a is:%d\n", a);
+    printf("The value of b is:%d\n", b);
+
     return NULL;
 }
 
@@ -18,7 +27,7 @@ int main()
 
     printf("Before thread use\n");
 
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 3; i++)
     {
         pthread_create(&thread1, NULL, thread, (void*)&thread1);
         pthread_join(thread1, NULL);
