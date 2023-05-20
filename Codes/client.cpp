60f0b0bf4429bd9a92e63fc92f0c830af8bc3e14
Mostafizur <bsse1320@iit.du.ac.bd>
2023-05-20 23:20:24 +0600

    client updated

--- Codes/client.cpp
+++ Codes/client.cpp
@@ -1,94 +1,101 @@
-/*
-filename server_ipaddress portno
-argv[0] -> file name
-argv[1] -> server_ipaddress
-argv[2 -> portno
-*/
+//without @/all working well
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
-int main(int argc, char *argv[])
-{
-    int portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    char buffer[255];
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 65425
+#define BUFFER_SIZE 1024
 
-    if(argc < 3)
-    {
-        printf("Usage %s hostname port\n", argv[0]);
-        exit(0);
-    }
-    
-    portno = atoi(argv[2]);
-    
-    int sock_desc = socket(AF_INET, SOCK_STREAM, 0);
-	if(sock_desc < 0)
-	{
-		printf("NO socket is created!\n");
-	}
-	else
-	{
-		printf("Socket is created!\n");
-	}
+void* receiveMessages(void* arg) {
+    int socket = *((int*)arg);
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, sizeof(buffer));
 
-    server = gethostbyname(argv[1]);
-    if(server == NULL);
-    {
-        fprintf(stderr, "Error, no such host...\n");
-    }
-
-    bzero((char*)&serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
-
-    if(connect(sock_desc, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("Connection failed\n");
-    }
-    else
-    {
-        printf("Connected....\n");
-    }
-    
-    while(1)
-    {
-        bzero(buffer, 255);
-        fgets(buffer, 255, stdin);
-        n = write(sock_desc, buffer, strlen(buffer));
-        if(n < 0)
-        {
-                printf("Error on writing\n");
+    while (1) {
+        int bytesRead = recv(socket, buffer, sizeof(buffer), 0);
+        if (bytesRead <= 0) {
+            printf("Disconnected from server.\n");
+            break;
         }
-       
 
-        bzero(buffer, 255);
-        n = read(sock_desc, buffer, 255);
-        if(n < 0)
-        {
-                printf("Error on reading\n");
-        }
-        
-        printf("Server:%s", buffer);
-        int i = strncmp("Bye", buffer, 3);
-        if(i == 0)
-        break;
+        printf("%s\n", buffer);
+
+        memset(buffer, 0, sizeof(buffer));
     }
-    
-    close(sock_desc);
-    
 
+    pthread_exit(NULL);
+}
 
+int main() {
+    int clientSocket;
+    struct sockaddr_in serverAddress;
+    pthread_t thread;
 
+    // Create client socket
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket < 0) {
+        perror("Error creating socket");
+        exit(1);
+    }
 
+    // Configure server address
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr(SERVER_IP);
+    serverAddress.sin_port = htons(SERVER_PORT);
 
+    // Connect to the server
+    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+        perror("Error connecting to server");
+        exit(1);
+    }
 
+    // Receive the initial prompt to enter username
+    char prompt[50];
+    memset(prompt, 0, sizeof(prompt));
+    int bytesRead = recv(clientSocket, prompt, sizeof(prompt), 0);
+    if (bytesRead > 0) {
+        printf("%s", prompt);
+    }
+
+    // Read the username from the user
+    char username[50];
+    fgets(username, sizeof(username), stdin);
+    username[strcspn(username, "\n")] = '\0';
+
+    // Send the username to the server
+    send(clientSocket, username, strlen(username), 0);
+
+    // Create a new thread to receive messages from the server
+    if (pthread_create(&thread, NULL, receiveMessages, &clientSocket) != 0) {
+        perror("Error creating thread");
+        exit(1);
+    }
+
+    // Read and send user messages
+    char message[BUFFER_SIZE];
+    while (1) {
+        fgets(message, sizeof(message), stdin);
+        message[strcspn(message, "\n")] = '\0';
+
+        // Check if user wants to exit
+        if (strcmp(message, "/exit") == 0) {
+            printf("Disconnected from server.\n");
+            break;
+        }
+
+        // Send the message to the server
+        send(clientSocket, message, strlen(message), 0);
+    }
+
+    // Close the client socket
+    close(clientSocket);
+
+    return 0;
 }
